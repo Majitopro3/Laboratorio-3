bce1cd89f9919f064883f484e8e0f2ae6efc7d1b
Maria José <mariabuitrago.jo@academia.umb.edu.co>
2024-03-24 18:04:30 +0000

    ecg

--- ecg.m
+++ ecg.m
@@ -1,1 +1,21 @@
-m
+ecgfetos = edfread('r07.edf');
+info = edfinfo('r07.edf');
+info.SignalLabels
+fs = info.NumSamples/seconds(info.DataRecordDuration);
+recnum = 1;
+signum = 1;
+t = (0:info.NumSamples(signum)-1)/fs(signum);
+v = ecgfetos.(signum){recnum};
+plot(t,v)
+legend(strcat("Record ",int2str(recnum),", Signal ",info.SignalLabels(signum)))
+hold on
+recnum = 5;
+signum = 2;
+t = (0:info.NumSamples(signum)-1)/fs(signum);
+v = ecgfetos.(signum){recnum};
+
+plot(t,v, ...
+    'DisplayName',strcat("Record ",int2str(recnum),", Signal ",info.SignalLabels(signum)))
+hold off
+xlabel('t (seconds)')
+ylabel('v (microvoltios')
