239748bbb4c5c857b90475505aeddbfea8250aa5
Maria José <mariabuitrago.jo@academia.umb.edu.co>
2024-03-24 02:41:46 +0000

    correccion

--- Declararmatrices.m
+++ Declararmatrices.m
@@ -1,10 +1,18 @@
+fmt = format("shortG");
 A = randi([0 10],4,4)
 B = randi([0 10],4,4)
 suma = A+B
 resta = A-B
 producto= A*B
-transA = A.'
-transB = B.'
-operacion1= transA*B
-operacion2= transB*A
+inverA = inv(A)
+inverB = inv(B)
+operacion1= inverA*B
+operacion2= inverB*A
 potencia = A^2
+productop = A.*B
+division1 = A.\B
+division2 = A./B
+potencia2 = A.^B
+for i = 1:10
+  potencia2 = potencia2*10;
+end
