8ea5e7da1454e20d7dfee690b14701e02e958ec6
Camila <mariaangarita.ca@academia.umb.edu.co>
2024-03-24 22:58:01 +0000

    ejercicio tres

--- graficos.m
+++ graficos.m
@@ -2,16 +2,16 @@
 x = linspace(0, 10, 1000);
 f = (exp(x)/100)+(100* sin(x));
 g = x.^3 - 10 * x.^2 + 5 * x + 20;
-h = 2;
 
 plot(x,f, 'b-', 'LineWidth', 2);
 hold on;
-plot(x,h,'c-','LineWidth', 2);
-hold off;
+plot(x,g,'c','LineWidth', 2);
+hold on;
 
 xlabel('x');
 ylabel('y');
-title('Grafica de f(x) = (e^x /100)+(100 * sen(x))');
+title('Grafica de dos funciones');
 grid on;
+legend( {'(exp(x)/100)+(100* sin(x))', 'x.^3 - 10* x.^2 + 5*x + 20'}, 'Location','northwest')
 yline(0,'k--', 'LineWidth', 1.5,'DisplayName', 'y=0');
-hold off;
+
